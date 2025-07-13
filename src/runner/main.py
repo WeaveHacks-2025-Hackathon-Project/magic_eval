22345423e7bee13c59aa07fcee8055677df5c958
Manish Saroya <manishsaroya@gmail.com>
2025-07-12 17:52:43 -0700

    added crewai agent and defined its task

---
+++ src/runner/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+from scenario.crew import 
+
+
+def run():
+    # Replace with your inputs, it will automatically interpolate any tasks and agents information
+    inputs = {
+        'Store Location': 'OneDrive',
+    }
+    result = Scenario_Eval_Crew().crew().kickoff(inputs=inputs)
+    print(result)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        'Store Location': 'OneDrive',
+    }
+    try:
+        Scenario_Eval_Crew().crew().train(n_iterations=int(sys.argv[1]), inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
---
+++ src/runner/scenario.py
@@ -0,0 +1,45 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+
+# Uncomment the following line to use an example of a custom tool
+# from surprise_travel.tools.custom_tool import MyCustomTool
+
+# Check our tools documentation for more information on how to use them
+from pydantic import BaseModel, Field
+
+class Scenario(BaseModel):
+    name: str = Field(..., description="Name of the scenario")
+    description: str = Field(..., description="Description of the scenario")
+    why_its_suitable: str = Field(..., description="Why it's a suitable scenario")
+
+@CrewBase
+class Scenario_Eval_Crew():
+    """Scenario evaluation crew"""
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
+
+    @agent
+    def scenario_evaluator(self) -> Agent:
+        return Agent(
+            config=self.agents_config['scenario_evaluator'],
+            verbose=True,
+            allow_delegation=False,
+        )
+
+    @task
+    def scenario_evaluator_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['scenario_evaluator_task'],
+            agent=self.scenario_evaluator()
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the SurpriseTravel crew"""
+        return Crew(
+            agents=self.agents, # Automatically created by the @agent decorator
+            tasks=self.tasks, # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=2,
+            # process=Process.hierarchical, # In case you want to use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
