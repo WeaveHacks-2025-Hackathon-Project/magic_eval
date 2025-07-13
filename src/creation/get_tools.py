58efbf103d80d6c27c27d349c510154124a42603
Gururaj Deshpande <66385690+guru-desh@users.noreply.github.com>
2025-07-13 11:25:15 -0700

    add more boilerplate for haarish

--- src/creation/get_tools.py
+++ src/creation/get_tools.py
@@ -3,4 +3,5 @@ from google.adk.agents import Agent
 
 def get_tools_from_agent(adk_agent: Agent) -> SOMETHING:
     # TODO: For Harish, we need to prompt the adk agent and tell it to return a list of tools and their descriptions
+    # Use the call_agent_async function to run the ADK agent
     raise NotImplementedError("Not implemented")
